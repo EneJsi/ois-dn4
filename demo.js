8692d5d7ce9487e31edb05e7fb69b49d453c6f64
Stoklas Nac <ns1844@student.uni-lj.si>
2014-12-14 13:09:31 +0000

    Testi krogcev!

--- demo.js
+++ demo.js
@@ -332,8 +332,8 @@ function BmiIzracun() {
 				    }
 				   
 					$("#rezultatBMI").append(results);
-					var krogciRes = "<div class='col-lg-6 col-md-6 col-sm-6'><div class='panel panel-default'><div class='panel-heading'><div class='row'><div class='col-lg-6 col-md-6 col-sm-6'>Slovenija in vi</div></div></div>";				
-					krogciRes += "<div class='panel-body text-center'><svg width='120' height='120'><circle r='" +BMI*2+"'></circle></svg></div></div></div>";
+					var krogciRes = "<div class='col-lg-6 col-md-6 col-sm-6'><div class='panel panel-default'><div class='panel-heading'><div class='row'><div class='col-lg-6 col-md-6 col-sm-6'><b>Slovenija in vi</b></div></div></div>";				
+					krogciRes += "<div class='panel-body text-center'><svg width='120' height='120'><circle r='" +BMI+"'></circle></svg></div></div></div>";
 					$("#krogciKROGCIkrogciiiIII").append(krogciRes);
 					
 					
