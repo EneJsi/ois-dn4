f7ce7c70fdf02c8734a34b3498e8ea36c54d1eb2
Stoklas Nac <ns1844@student.uni-lj.si>
2014-12-14 12:22:14 +0000

    Koncano brez vizualizacije!

--- demo.js
+++ demo.js
@@ -330,8 +330,13 @@ function BmiIzracun() {
 							}else{
 				       			results += "<br><span class='label label-danger'>Imate prekomerno telesno težo. Bolje morate skrbeti zase!</span>";
 				    }
-				    console.log(results);
+				   
 					$("#rezultatBMI").append(results);
+					var krogciRes = "<div class='col-lg-6 col-md-6 col-sm-6'><div class='panel panel-default'><div class='panel-heading'><div class='row'><div class='col-lg-6 col-md-6 col-sm-6'><b>Pregled</b>Slovenija in vi</b> bolnika</div></div></div>";				
+					krogciRes += "<div class='panel-body'><svg><circle r='" + BMI*2 +"'></circle><circle r='" + sloBMI + "'></circle></svg></div></div></div>";
+					$("#krogciKROGCIkrogciiiIII").append(results);
+					
+					
 					}
 					    },
 					    error: function() {
